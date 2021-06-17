16ccfa9bfffc8ca37f89859eda4a1b6a21a6c442
Morgan <morgan.espindola@gmail.com>
2021-06-17 09:17:37 -0300

    teste vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBQ5Ds9b5GcLzqa1pNZgddEc7C0ddqxA4s",
+    authDomain: "prova-wk.firebaseapp.com",
+    databaseURL: "https://prova-wk-default-rtdb.firebaseio.com",
+    projectId: "prova-wk",
+    storageBucket: "prova-wk.appspot.com",
+    messagingSenderId: "949522644214",
+    appId: "1:949522644214:web:e8227da833f76b31bac0d7",
+    measurementId: "G-CH2P08NLSK"
+  }
 };
